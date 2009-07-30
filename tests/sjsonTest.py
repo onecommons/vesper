6968553143be7ea51a4c3f9d435c2f33e4edf24c
adam@souzis.com <adam@souzis.com>
2009-07-30 15:22:00 -0700

    better sjson input and output

--- tests/sjsonTest.py
+++ tests/sjsonTest.py
@@ -1,11 +1,17 @@
 from sjson import * 
 from pprint import pprint
 
-def assert_json_match(expected, result):
-    assert json.dumps(result,sort_keys=True) == json.dumps(expected, sort_keys=True), pprint(result)
+def assert_json_match(expected, result, dosort=False):
+    if dosort and isinstance(expected, list):
+        result.sort()
+        expected.sort()
+    result = json.dumps(result,sort_keys=True)
+    expected = json.dumps(expected, sort_keys=True)
+    assert result == expected, pprint((result, '!=', expected))
 
 def assert_stmts_match(expected_stmts, result_stmts):
-    assert set(result_stmts) == set(expected_stmts), (pprint(result_stmts), '!=', pprint(expected_stmts))
+    assert set(result_stmts) == set(expected_stmts), (
+                        pprint(result_stmts), '!=', pprint(expected_stmts))
 
 def assert_json_and_back_match(src, expectedstmts=None):
     test_json = [ json.loads(src) ]
@@ -21,7 +27,7 @@ def assert_json_and_back_match(src, expectedstmts=None):
 def assert_stmts_and_back_match(stmts, expectedobj = None):
     result = sjson()._to_sjson( stmts )
     if expectedobj is not None:
-        assert_json_match(expectedobj, result)
+        assert_json_match(expectedobj, result, True)
     
     result_stmts = sjson().to_rdf( result )
     assert_stmts_match(stmts, result_stmts)
@@ -53,28 +59,57 @@ def test():
     stmts.extend( 
         [Statement("http://example.org/book#2", 'test:sequelto' , 'http://example.org/book#1', OBJECT_TYPE_RESOURCE),]
     )
-    
-    expected = { 'results': ['http://example.org/book#1',
-                {'http://purl.org/dc/elements/1.1/title': 'Advanced SPARQL',
-                'id': 'http://example.org/book#2',
-                'test:sequelto' : 'http://example.org/book#1'}
-            ],'shared': {'http://example.org/book#1': {
-                'http://purl.org/dc/elements/1.1/description': 'A book about SPARQL',
-                'http://purl.org/dc/elements/1.1/title': 'SPARQL - the book',
-                'id': 'http://example.org/book#1'}
-                }
-            } 
-    
+
+    expected = [{"http://purl.org/dc/elements/1.1/title": "Advanced SPARQL",
+    "id": "http://example.org/book#2",
+    "test:sequelto": {
+        "http://purl.org/dc/elements/1.1/description": "A book about SPARQL",
+        "http://purl.org/dc/elements/1.1/title": "SPARQL - the book",
+        "id": "http://example.org/book#1"
+        }
+    }]
+
     assert_stmts_and_back_match(stmts, expected)
-    
+
+    src = '''
+    { "id" : "atestid",
+       "foo" : { "id" : "bnestedid", "prop" : "value" }
+    }'''
+    assert_json_and_back_match(src)
+
     src = '''
     { "id" : "testid", 
     "foo" : ["1","3"],
-       "bar" : [] 
+     "bar" : [],
+     "baz" : { "nestedobj" : { "id" : "anotherid", "prop" : "value" }}
     } 
     '''
+
     assert_json_and_back_match(src)
+
+    #test nested lists
+    src = '''
+    { "id" : "testid",
+    "foo" : [1,  ["nested1",
+                       { "id": "nestedid", "nestedprop" : [ "nested3" ] },
+                    "nested2"],
+            3],
+    "bar" : [ [] ]
+    }
+    '''
+    assert_json_and_back_match(src)
+
     #test numbers and nulls
+    src = '''
+    { "id" : "test",
+    "float" : 1.0,
+      "integer" : 2,
+      "null" : null,
+      "list" : [ 1.0, 2, null ]
+    }
+    '''
+    assert_json_and_back_match(src)
+
     #test missing ids and exclude_blankids
     #test shared
     print 'tests pass'
